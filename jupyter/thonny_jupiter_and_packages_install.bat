a809e90af556a44ec329c2c48077423d825091b4
Rune Langøy <rune.langoy@gmail.com>
2023-09-27 09:46:24 +0200

    Added jupiter install for Thonny windows users
    Added jupiter install for Thonny windows users

---
+++ jupyter/thonny_jupiter_and_packages_install.bat
@@ -0,0 +1,5 @@
+"C:\Program Files (x86)\Thonny\python.exe" -m pip install notebook
+"C:\Program Files (x86)\Thonny\python.exe" -m pip install chipwhisperer
+"C:\Program Files (x86)\Thonny\python.exe" -m pip install ipywidgets
+"C:\Program Files (x86)\Thonny\python.exe" -m pip install matplotlib
+"C:\Program Files (x86)\Thonny\python.exe" -m pip install ipympl
---
+++ jupyter/thonny_start_jupiter.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Thonny\python.exe" -m notebook  "Voltage Glitching CWNano Mod Th.ipynb"
